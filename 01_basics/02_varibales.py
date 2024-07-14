cbd867e93f500aa5802a9759eeb5b2802bb4f719
azimiHamid <azimihamid2003@gmail.com>
2024-07-14 16:02:07 +0500

    Add variables.py

---
+++ 01_basics/02_varibales.py
@@ -0,0 +1,12 @@
+# Variables and dataTypes: 
+# String(str), 
+# integer(int), 
+# float(float), 
+# boolean(bool)
+
+name = "Hamid"
+age = 21
+salary = 21.3
+is_online = True
+
+print(f"Hi, My name is {name}, I am {age} years old, I earn {salary}$ per day.")
