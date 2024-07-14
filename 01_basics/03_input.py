46adf7cdb8966237f12e67ea948328d0b93257ff
azimiHamid <azimihamid2003@gmail.com>
2024-07-14 16:38:13 +0500

    Add titles for input.py file

--- 01_basics/03_input.py
+++ 01_basics/03_input.py
@@ -1,3 +1,10 @@
+'''
+What we will learn here:
+    1. input() 
+    2. round()
+    3. function
+'''
+
 # input : We use input() method to get input from the user.
 # Note: input() method always convert everything as a string. So if we input a number like 2, it's going to be processed as "2" not 2. And for this cases we can change the types of variables using Datatype conversion for example int(input("...")) or float(input("..."))
 
