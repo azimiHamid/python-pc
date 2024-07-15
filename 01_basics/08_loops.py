b550700c560cacf1a307870ac43404e85ea2b3eb
azimiHamid <azimihamid2003@gmail.com>
2024-07-15 15:50:20 +0500

    Update loops file by adding nested loops

--- 01_basics/08_loops.py
+++ 01_basics/08_loops.py
@@ -7,12 +7,23 @@ while i <= 10:
 
 
 
+
 print("\n---------------------- for loop -------------------------")
 # ////////////////////////// for loop ///////////////////////////
+for letter in "Python\n":
+    print(letter)
+
 
 numbers = [1, 2, 3, 4, 5]
 for num in numbers:
     print(num)
+print("\n")
+
+prices = [20, 30, 40]
+total = 0
+for p in prices:
+    total += p
+print(f"Total price : {total}")
 
 
 print("\n---------- let's do it in while loop -----------")
@@ -35,5 +46,20 @@ for n in num:
 
 
 print("\n")
-for x in range(1, 10, 2): 
-    print(f"{x}. Hello")
+for x in range(0, 10, 2): 
+    print(f"{x} x 2 = {x*2}")
+
+
+
+
+print("\n---------------- Nested loops -----------------")
+# Nested loops
+coordinates = []
+for x in range(3):
+    for y in range(3):
+        coordinates += [(x, y)]
+print(coordinates)
+
+# Nested loops by using (list comprehension) method
+pairs = [(x, y) for x in range(3) for y in range(3)]
+print(pairs)
