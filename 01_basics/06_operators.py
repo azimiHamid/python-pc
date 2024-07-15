9e463cd3b2eae0e018a9ebfd6f4b4b1bbc582c70
azimiHamid <azimihamid2003@gmail.com>
2024-07-15 13:33:46 +0500

    Update operators file and add Math operator

--- 01_basics/06_operators.py
+++ 01_basics/06_operators.py
@@ -1,3 +1,4 @@
+print("\n----------------- Arithmetic Operators ------------------")
 # Arithmetic Operators:
 
 print(2+3)
@@ -12,10 +13,14 @@ print(10/3)
 # Modulus (%)
 print(4%3)
 
-print(round(8.9))
+print(round(8.7))
 print(round(8.3))
 
 
+
+
+print("\n----------------- Augmented/Enhanced Operator ------------------")
+# Augmented/Enhanced Operator
 x = 4
 
 x += 3  #==> x = x + 3  now x is : 7
@@ -31,6 +36,9 @@ x /= 2  # now x is : 5.25
 print(x)
 
 
+
+
+print("\n----------------- Operators Precedence ------------------")
 # /////////////////////// Operators Precedence /////////////////////////////////
 # PEMDAS: 
 # Parenthessis
@@ -46,6 +54,23 @@ print(a)
 
 
 
+print("\n----------------- Math Operator ------------------")
+# /////////////////////// Math Operator /////////////////////////////////
+import math
+
+print(abs(-23.5))
+print(round(abs(-23.486)))
+
+number = 4.8973
+print(math.floor(number))
+print(math.ceil(number))
+print(math.factorial(5))
+
+
+
+
+
+print("\n----------------- Comparison Operators ------------------")
 # ////////////////////////// Comparison Operators //////////////////////////////
 #   > , >= , < , <= , == , !=
 
@@ -58,10 +83,22 @@ print(c)
 
 
 
-
+print("\n----------------- Logical Operators ------------------")
 # ////////////////////////// Logical Operators //////////////////////////////////
 # and , or , not
 
 price = 25
 print(price > 10 and price < 30)
 print(not price > 2)
+
+
+has_haigh_income = True
+has_good_credit = False
+
+# and : both the conditions should be true
+if has_haigh_income and has_good_credit:
+    print("\nEligible for loan.\n")
+
+# or : at least one of the conditions should be true
+if has_haigh_income or has_good_credit:
+    print("\nEligible for loan.\n")
