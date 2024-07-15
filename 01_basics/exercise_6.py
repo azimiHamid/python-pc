fd69182592576d21bf428a86be0ccc8e9de14566
azimiHamid <azimihamid2003@gmail.com>
2024-07-15 18:07:44 +0500

    update the exercise_6 file

--- 01_basics/exercise_6.py
+++ 01_basics/exercise_6.py
@@ -1,13 +1,25 @@
 # Find the duplicates in a list and remove them:
 
+names = ["hamid", "hamid", "Hadi", "Hadi", "hamid", "Ahmad", "Hadi", "Ali"]
 numbers = [2, 2, 4, 6, 4, 5, 7, 7, 2, 6, 1, 3, 4, 3]
-print(numbers)
 
-uniques = []
-
+unique_numbers = []
 for num in numbers:
-    if num not in uniques:
-        uniques.append(num)
+    if num not in unique_numbers:
+        unique_numbers.append(num)
 
-uniques.sort()
-print(uniques)
+print(numbers)
+unique_numbers.sort()
+print(unique_numbers)
+
+
+print("\n----------------------------------")
+
+unique_names = []
+for name in names:
+    if name not in unique_names:
+        unique_names.append(name)
+
+print(names)
+unique_names.sort()
+print(unique_names)
