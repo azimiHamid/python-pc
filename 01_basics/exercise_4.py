a411de5e041fa59ff8c7229c6e79c4c7a2d16622
azimiHamid <azimihamid2003@gmail.com>
2024-07-15 16:35:55 +0500

    update exercise_4, adding more patterns and use list comprehension

--- 01_basics/exercise_4.py
+++ 01_basics/exercise_4.py
@@ -9,6 +9,14 @@ for i in numbers:
     print(output)
 
 
+print("\n---- use list comprehension ----")
+# use list comprehension:
+lines = ["".join(["X" for _ in range(i)]) for i in numbers]
+pattern = "\n".join(lines)
+print(pattern)
+
+
+
 print("\n------------------ Print H --------------------")
 # Print H
 rows = [5, 5, 5, 5, 5]  # Each row should have 5 positions
@@ -18,3 +26,11 @@ for i in range(len(rows)):
         print("XXXXX")
     else:
         print("X   X")
+
+
+print("\n---- use list comprehension ----")
+# use list comprehension:
+pattern = ["XXXXX" if i == 2 else "X   X" for i in range(5)]
+
+for line in pattern:
+    print(line)
