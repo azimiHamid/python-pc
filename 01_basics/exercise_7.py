c42024af656b09c61a6f115f707404575fd95b66
azimiHamid <azimihamid2003@gmail.com>
2024-07-16 12:36:23 +0500

    Update exercise_7 an add emoji-converter project to it

--- 01_basics/exercise_7.py
+++ 01_basics/exercise_7.py
@@ -1,5 +1,5 @@
 # Project #1 of dictionaries
-phone = input("Phone : ")
+phone = input("Phone : ").replace(" ", "")
 digits_mapping = {
     "1": "One",
     "2": "Two",
@@ -13,20 +13,38 @@ digits_mapping = {
     "0": "Zero"
 }
 
-output = ""
-for char in phone:
-    output += digits_mapping.get(char, "!") + " "
+# output = ""
+# for char in phone:
+#     output += digits_mapping.get(char, "!") + " "
 
-print(output)
+# print(output)
+
+for num in digits_mapping:
+    if num in phone:
+        phone = phone.replace(num, digits_mapping.get(num) + " ")
+
+print(phone)
 
 
 
 
-print("--------------- project #2 -------------------")
+print("\n--------------- project #2 -------------------")
 # Project #2 of dictionaries -> emoji converter
 
-emoji = {
+emojis = {
     ":)": "😊",
     ":(": "😔",
+    ":D": "😁",
+    ":P": "😜",
+    ";)": "😉",
+    ":o": "😮",
 }
 msg = input("> ")
+
+for emoji in emojis:
+    if emoji in msg:
+        msg = msg.replace(emoji, emojis[emoji])
+    else:
+        msg = "⛔"
+        
+print(msg)
