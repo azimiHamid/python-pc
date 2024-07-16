ab8a4f9fcf724b8c20d55edc2cacb9f40fe2962c
azimiHamid <azimihamid2003@gmail.com>
2024-07-16 17:39:29 +0500

    Add main.py and it's related converter_modules.py

---
+++ 02_advance/03_main.py
@@ -0,0 +1,8 @@
+# method 1 - importing a module
+import converter_modules
+print(converter_modules.kg_to_lbs(83))
+
+
+# method 2 - importing a module
+from converter_modules import kg_to_lbs
+print(kg_to_lbs(75))
