8688631d49133d7ca9b52c549b66755e9b00b1cc
azimiHamid <azimihamid2003@gmail.com>
2024-07-16 13:16:14 +0500

    update function file, adding return keyword to it

--- 01_basics/12_function.py
+++ 01_basics/12_function.py
@@ -7,6 +7,7 @@ def greet():
 greet()
 
 
+
 print("\n------------------ function with parameter --------------------")
 def greet_with(name, age, city):
     print(f"Hey, {name}!")
@@ -18,11 +19,21 @@ greet_with(21, "Kabul", "Hamid")
 
 
 
+
 print("\n------------------ function keyword arguments --------------------")
-def greet_with(name, age, city):
+def greetings(name, age, city):
     print(f"Hey, {name}!")
     print(f"You are {age} years old.")
     print(f"How is the weather in {city}?")
     
     
-greet_with(city="Kabul", name="Hamid", age=21)
+greetings(city="Kabul", name="Hamid", age=21)
+
+
+
+
+print("\n------------------ return keyword --------------------")
+def sum(a, b):
+    print(a + b)
+
+print(sum(3, 4)) # output: 7  and  None : why None? because in Python, by-default, all the functions return None.
